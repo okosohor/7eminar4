0c93a92db02289f2dcea35825d12a562dfee59f3
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-10-02 15:23:29 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
   app: {
     baseURL: '/7eminar/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets',
