6dabb65f42ceb976e53a354eb8ffc817be7727c1
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-10-02 03:49:57 +0300

    add git wf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  
+  // app: {
+  //   baseURL: '/nuxt-github-pages/', // baseURL: '/<repository>/'
+  //   buildAssetsDir: 'assets',
+  // },
   modules: [ 'nuxt-swiper',
     ['@nuxtjs/google-fonts', {
       families: {
